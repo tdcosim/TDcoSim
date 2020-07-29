7458dfc4883a976077b73025bcf19f9b4f85aa91
Karthik <kbalasubramaniam@anl.gov>
2020-07-29 15:59:16 -0500

    added additional checks and cleanup.

--- tdcosim/procedure/procedure.py
+++ tdcosim/procedure/procedure.py
@@ -1,7 +1,11 @@
+import os
+import sys
+
 from default_procedure import DefaultProcedure
 from default_static_procedure import DefaultStaticProcedure
 from default_dynamic_procedure import DefaultDynamicProcedure
 from tdcosim.global_data import GlobalData
+
 class Procedure(DefaultProcedure):
 	def __init__(self):
 		if GlobalData.config['simulationConfig']['simType'].lower() == 'static':
@@ -14,5 +18,11 @@ class Procedure(DefaultProcedure):
 	def simulate(self):
 		self._procedure.setup()	
 		self._procedure.initialize()
-		self._procedure.run()	
+		self._procedure.run()
+		if 'cleanup' in GlobalData.config['psseConfig']:
+			for entry in GlobalData.config['psseConfig']['cleanup']:
+				if 'win' in sys.platform:
+					os.system('del {}'.format(entry))
+				if 'linux' in sys.platform:
+					os.system('rm {}'.format(entry))
 
