4103b5945c19f0c6d4d277b02573692fcd07c6a9
Sam <yim@anl.gov>
2020-10-12 14:36:55 -0500

    update the test suite for test

--- test_suite.py
+++ test_suite.py
@@ -26,7 +26,6 @@ def suite():
                                                
     return suite
 
-if __name__ == '__main__':
-        
+if __name__ == '__main__':    
     runner = unittest.TextTestRunner()
     runner.run(suite())
