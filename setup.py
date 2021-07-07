6ce9f2eea238a3a3350434f76d6caab876ee2d4a
Balasubramaniam <kbalasubramaniam@anl.gov>
2021-07-07 16:40:20 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ if platform.architecture()[0]=='64bit':
       author = 'TDcoSim Team',
       author_email='yim@anl.gov',
       license= 'LICENSE.txt',
-      install_requires=['pywin32==228','matplotlib>=2.0.2','numpy==1.16.2','scipy==1.2.1',
-      'xlsxwriter==1.1.8','psutil==5.7.0','pandas==0.24.2'],
+      install_requires=['pywin32==228','matplotlib>=2.0.2','numpy>=1.16.2','scipy>=1.2.1',
+      'xlsxwriter==1.1.8','psutil==5.7.0','pandas>=0.24.2'],
       )
 else:
 	setup(name='tdcosim',
@@ -20,7 +20,7 @@ else:
       author = 'TDcoSim Team',
       author_email='yim@anl.gov',
       license= 'LICENSE.txt',
-      install_requires=['pywin32==224','matplotlib==2.0.2','numpy==1.16.2','scipy==1.2.1',
-      'xlsxwriter==1.1.8','psutil==5.7.0','pandas==0.24.2'],
+      install_requires=['pywin32==224','matplotlib>=2.0.2','numpy>=1.16.2','scipy>=1.2.1',
+      'xlsxwriter==1.1.8','psutil==5.7.0','pandas>=0.24.2'],
       )
 
