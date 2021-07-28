c151f7a8416468c3de3651d3d1345d2f7d49130f
sam <yim@anl.gov>
2021-07-28 15:47:28 -0500

    update gis and table for dashboard

--- tdcosim/dashboard/app.py
+++ tdcosim/dashboard/app.py
@@ -5,6 +5,7 @@ import pdb
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 import dash_table
 import plotly.express as px
 import pandas as pd
@@ -14,7 +15,14 @@ from tdcosim.dashboard import Dashboard
 
 
 app = dash.Dash(__name__)
+PIQIDIR=os.path.dirname(os.path.abspath(__file__))
+PIQIDIR=PIQIDIR + "\\vizsample"
+print(PIQIDIR)
+df=pd.read_csv(open(os.path.join(PIQIDIR,'dataframe.csv')),compression=None)
 helper=Dashboard()
+df = helper.add_lat_lon_to_df(df, 'tnodeid')
+print('mapdf')
+
 
 objects={'graph':{},'tab':{},'tabs':{},'upload':{},'textarea':{},'dropdown':{},'selection':{},'df':{}}
 def create_graphs(objects,objID,title,fontColor,xlabel,ylabel):
@@ -25,6 +33,19 @@ def create_graphs(objects,objID,title,fontColor,xlabel,ylabel):
 		objects['graph'][thisObjID].figure['layout']['xaxis']={'title':thisXlabel}
 		objects['graph'][thisObjID].figure['layout']['yaxis']={'title':thisYlabel}
 
+def create_map(df, thisObjID):	
+	objects['map'] = {
+	 thisObjID : helper.map_template(df)	
+	}
+def create_table(df, thisObjID):	
+	objects['table'] = {
+	 thisObjID : helper.table_template(df)	
+	}
+
+def create_filter(df, thisObjID):
+	objects['filter'] = {
+	 thisObjID : helper.filter_template(df, 'tnodeid', [])	
+	}
 
 def create_tab(objects,objID,label,style=None,selected_style=None):
 	if not style:
@@ -39,19 +60,66 @@ def create_tabs(objects,objID,children):
 	objects['tabs'][objID]=dcc.Tabs(id=objID,children=children)
 
 
+#======================================================================================================================
+create_map(df, 'gis')
+create_table(df, 'table')
+create_filter(df, 'filter')
+create_graphs(objects,['vmag','freq'],['vmag','freq'],['white','white'],['Time (s)', 'Time (s)'],\
+['Vmag (PU)','f (hz)'])
+
+create_tab(objects,['gis','table','analytics'],['GIS','Table','Analytics'])
+
+create_tabs(objects,'main_tab',[objects['tab']['gis'],objects['tab']['table'],objects['tab']['analytics']])
+objects['tab']['gis'].children=[
+	objects['map']['gis']
+]
+objects['tab']['table'].children=[
+	objects['table']['table']
+]
+objects['tab']['analytics'].children=[
+objects['filter']['filter'],
+html.Div(objects['graph']['freq'],style={'display':'inline-block','width':'49.9%','height':'98vh'}),
+html.Div(objects['graph']['vmag'],style={'display':'inline-block','width':'49.9%','padding':'1% 0% 0% .2%',\
+'height':'98vh'})]
+
+app.layout=html.Div(objects['tabs']['main_tab'],
+style={'width':'99vw','height':'98vh','background-color':'rgba(0,0,0,.8)'})
 
 #======================================================================================================================
-if __name__ == '__main__':
-	create_graphs(objects,['vmag','freq'],['vmag','freq'],['white','white'],['Time (s)', 'Time (s)'],\
-	['Vmag (PU)','f (hz)'])
-	create_tab(objects,['gis','table','analytics'],['GIS','Table','Analytics'])
-	create_tabs(objects,'main_tab',[objects['tab']['gis'],objects['tab']['table'],objects['tab']['analytics']])
-	objects['tab']['analytics'].children=[
-	html.Div(objects['graph']['freq'],style={'display':'inline-block','width':'49.9%','height':'98vh'}),
-	html.Div(objects['graph']['vmag'],style={'display':'inline-block','width':'49.9%','padding':'1% 0% 0% .2%',\
-	'height':'98vh'})]
+@app.callback(	
+		Output('unselectednodesel', 'children'),
+		Output('selectednodesel', 'children'),
+		Output('columndrop', 'options'),
+		Input('addbtn', 'n_clicks'),
+		Input('removebtn', 'n_clicks'),
+		Input('unselectednodesel', 'children'),
+		Input('selectednodesel', 'children'),
+		prevent_initial_call=True 
+	)
+def click_addbtn(addn_clicks, remove_clicks, unselectoptions, selectedoptions):		
+	changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+	selectednode = []
+	if 'addbtn' in changed_id:	
+		for x in selectedoptions:		
+			selectednode.append(x['props']['value'])
+		for x in unselectoptions:		
+			if 'n_clicks' in x['props']:
+				selectednode.append(x['props']['value'])
+		addbtnclick = addn_clicks
+	elif 'removebtn' in changed_id:
+		for x in selectedoptions:
+			if 'n_clicks' not in x['props']:	
+				selectednode.append(x['props']['value'])
+		removebtnclick = remove_clicks
+			
 
-	app.layout=html.Div(objects['tabs']['main_tab'],
-	style={'width':'99vw','height':'98vh','background-color':'rgba(0,0,0,.8)'})
+	unselectedchildren, selectedchildren, columnoptions = helper.update_filter(df, 'tnodeid', selectednode)
+
+	return unselectedchildren, selectedchildren, columnoptions
+
+
+
+#======================================================================================================================
+if __name__ == '__main__':	
 	# http://localhost:8050/
 	app.run_server(debug=True,use_reloader=True)
--- tdcosim/dashboard/dashboard.py
+++ tdcosim/dashboard/dashboard.py
@@ -4,6 +4,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
+import numpy as np
+import math
+import plotly.express as px
 
 
 class Dashboard(object):
@@ -81,4 +84,101 @@ class Dashboard(object):
 			return dropdown
 		except:
 			LogUtil.exception_handler()
+	def generate_lat_lon(self,nPts,latMin=33,latMax=44,lonMin=-115,lonMax=-80):
+		try:
+			res={}
+			res['lat']=np.array([np.random.random()*(latMax-latMin)+latMin for entry in range(nPts)])
+			res['lon']=np.array([np.random.random()*(lonMax-lonMin)+lonMin for entry in range(nPts)])
+
+			return res
+		except:
+			raise
+
+	def add_lat_lon_to_df(self,df,uniqueColumnID):
+		try:
+			if 'lat' not in df.columns:
+				df=df.assign(lat=[0]*df.shape[0])
+			if 'lon' not in df.columns:
+				df=df.assign(lon=[0]*df.shape[0])
+
+			for entry in set(df[uniqueColumnID]):								
+			 	res=self.generate_lat_lon(nPts=1)	
+			 	tempd = df[df[uniqueColumnID]==entry].copy()
+			 	df.loc[df[uniqueColumnID]==entry, 'lat'] = [res['lat'][0] for i in range(tempd.shape[0])]
+			 	df.loc[df[uniqueColumnID]==entry, 'lon'] = [res['lon'][0] for i in range(tempd.shape[0])]	 	
+
+			return df
+		except:
+			raise
+	def map_template(self, df):
+		myMap = px.scatter_mapbox(		
+			data_frame=df, 
+			lat="lat", 
+			lon="lon", 
+			hover_name=None, 
+			hover_data=["tnodeid"],
+			color_discrete_sequence=["fuchsia"], 
+			zoom=3,
+			width=900,
+			height=300	
+			)
+
+		myMap.update_layout(mapbox_style='stamen-toner')
+		myMap.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+		mapObj=dcc.Graph(id="mapObj",figure=myMap,clear_on_unhover=True, style={"width": "48vw", "height":"30vh",'display':'inline-block', 'vertical-align':'top', 'padding':'20px 0vw .5vh .5vw'})
+		return mapObj
+
+	def table_template(self, df):
+		myTable = dash_table.DataTable(
+			id='myTable',
+			columns=[{"name": i, "id": i} for i in df.columns],
+			data=df.to_dict('records'),
+			editable=False,
+			filter_action="native",
+			sort_action="native",
+			sort_mode="multi",		
+			selected_columns=[],
+			selected_rows=[],
+			page_action="native",
+			page_current= 0,
+			page_size= 14,
+			style_data_conditional=[
+			{'if': {'row_index': 'odd'},'backgroundColor': 'rgb(248, 248, 248)'},
+			{'if': {'row_index': 'even'},'backgroundColor': 'rgba(0, 0, 0,.9)','color':"white"}],
+			style_header={'backgroundColor': 'rgb(200, 200, 200)','fontWeight': 'bold','fontSize':20})
+		return myTable
+	
+	def update_filter(self, df, uniqueColumnID, selectednode):
+		tnodeids = set(df[uniqueColumnID])
+		unselectedchildren = []
+		selectedchildren = []
+		columnoptions = []
+		for node in tnodeids:
+			node = str(node)			
+			if node in selectednode:
+				selectedchildren.append(
+					html.Option(value=str(node), children=[node])
+					)
+			else:
+				unselectedchildren.append(
+					html.Option(value=str(node), children=[node])
+					)		
+
+		return unselectedchildren, selectedchildren, columnoptions
+	def filter_template(self, df, uniqueColumnID, selectednode):
+		unselectedchildren, selectedchildren, columnoptions = self.update_filter(df, uniqueColumnID, selectednode)
+
+		unselectednode = html.Select(multiple=True, id="unselectednodesel", children=unselectedchildren)
+		nodeaddbtn = html.Button("==>", id='addbtn')
+		noderemovebtn = html.Button("<==", id='removebtn')
+		selectednode = html.Select(multiple=True, id="selectednodesel", children=selectedchildren)
+		columndrop = dcc.Dropdown(id="columndrop",options=columnoptions)
+		div = html.Div([
+			unselectednode,
+			nodeaddbtn,
+			noderemovebtn,
+			selectednode,
+			columndrop
+			])
+		return div
 
