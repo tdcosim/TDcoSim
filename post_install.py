0772bd9fec052d484dc2330018223b6de4ed1ef4
Balasubramaniam <kbalasubramaniam@anl.gov>
2021-10-06 09:00:46 -0500

    patch for pssePath

--- post_install.py
+++ post_install.py
@@ -2,10 +2,12 @@ import os
 import sys
 import inspect
 
+import pvder
+
 
 if __name__=="__main__":
-	baseDir=os.path.dirname(inspect.getfile(os))
-	tdcosimapp=os.path.join(baseDir,'site-packages','tdcosim','tdcosimapp.py')
+	baseDir=os.path.dirname(os.path.dirname(inspect.getfile(pvder)))
+	tdcosimapp=os.path.join(baseDir,'tdcosim','tdcosimapp.py')
 	assert os.path.exists(tdcosimapp)
 	pyExe=sys.executable.split('\\')[-1].replace('.exe','')
 	directive='reg add "HKEY_CURRENT_USER\Software\Microsoft\Command Processor" /v AutoRun /d "doskey tdcosim={} \"{}\" $*"'.format(pyExe,tdcosimapp)
