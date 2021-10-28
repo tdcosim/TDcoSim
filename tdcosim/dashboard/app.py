e70fcca57022fde45b2bc6e346d90673f027e8ba
Plathottam <sibyjackgrove@gmail.com>
2021-10-28 15:47:20 -0500

    Add support for loading multiple csv/pkl loading and Dask

--- tdcosim/dashboard/app.py
+++ tdcosim/dashboard/app.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pickle
 import pdb
-
+import time
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -12,8 +12,8 @@ import plotly.express as px
 import pandas as pd
 import networkx as nx
 import numpy as np
-
-
+import dask.dataframe as dd
+import glob
 app = dash.Dash(__name__)
 
 
@@ -64,7 +64,7 @@ def create_tabs(objects,objID,children):
 def gather_objects():
 	# dropdown objects
 	objects['dropdown']=helper.filter_dropdown(columnFilter=['tnodeid','tnodesubid','dnodeid','property','scenario'],
-	multi=['tnodeid','tnodesubid','dnodeid'])
+	multi=['tnodeid','tnodesubid','dnodeid','scenario'])
 
 	objects['dropdown']['bubble_property']=dcc.Dropdown(id='bubble_property',\
 		options=[{'label':entry,'value':entry} for entry in ['VOLT']],value='',\
@@ -80,6 +80,11 @@ def gather_objects():
 
 	# create required objects
 	create_map(objects['dfMap'], 'gis')
+	df = objects['df']
+	
+	if isinstance(df,dd.core.DataFrame):
+		df = objects['df_scenario'] #Only show one scenario in table when using Dask
+	
 	create_table(df, 'table')
 	create_graphs(objects,['vmag','freq'],['vmag','freq'],['white','white'],['Time (s)', 'Time (s)'],\
 	['Vmag (PU)','f (hz)'])
@@ -103,6 +108,7 @@ def gather_objects():
 
 	# plots
 	objects['graph']['vmag'].style['height']='70vh'
+	
 	objects['tab']['plots'].children=[
 		html.Div(style={'display':'flex','flexDirection':'column','justifyContent':'space-around'},children=[
 			html.Div(children=[objects['dropdown']['scenario'],objects['dropdown']['tnodeid'],\
@@ -128,9 +134,15 @@ def update_plot_filter(tnodeid):
 			res=[]
 			for thisTnodeid in tnodeid:
 				if not res:
-					res=set(df[df.tnodeid==thisTnodeid].property)
+					if isinstance(df,dd.core.DataFrame):
+						res=set(list(objects['df_temp'][objects['df_temp'].tnodeid==thisTnodeid].property.unique()))
+					else:
+						res=set(df[df.tnodeid==thisTnodeid].property)
 				else:
-					res=res.intersection(set(df[df.tnodeid==thisTnodeid].property))
+					if isinstance(df,dd.core.DataFrame):
+						res=res.intersection(set(list(objects['df_temp'][objects['df_temp'].tnodeid==thisTnodeid].property.unique())))
+					else:
+						res=res.intersection(set(df[df.tnodeid==thisTnodeid].property))
 
 			property_options=[]
 			for entry in res:
@@ -149,31 +161,41 @@ Input('tnodesubid', 'value'),Input('property', 'value')])
 def update_plot(scenario,tnodeid,dnodeid,tnodesubid,prop):
 	try:
 		df=objects['df']
+		
 		df=df[df.t>=0]
 		figure=objects['graph']['vmag'].figure
 		style=objects['graph']['vmag'].style
 		if 'height' in figure['layout']:
 			figure['layout'].pop('height')
-
+		
 		if not isinstance(tnodeid,list):
 			tnodeid=[tnodeid]
-
-		if scenario:
-			df=df[df.scenario==scenario]
-
+		if not isinstance(scenario,list):
+			scenario=[scenario]
+		
 		if tnodesubid:
 			df=df[df.tnodesubid==tnodesubid[0]]####
-
+		tic = time.clock()
 		thisData=[]
 		if prop:
-			df=df[df.property==prop]
+			if isinstance(df,dd.core.DataFrame):
+				df_temp=df[(df.property==prop) & (df.tnodeid.isin(tnodeid)) & (df.scenario.isin(scenario))].compute()
+			else:
+				df=df[df.property==prop]
 			for thisTnodeid in tnodeid:
-				filterDF=df[df.tnodeid==thisTnodeid]
-				thisData.append({'x':filterDF.t, 'y':filterDF.value, 'type': 'chart','name':thisTnodeid})
+				for thisScenario in scenario:
+					if isinstance(df,dd.core.DataFrame):
+						filterDF=df_temp[(df_temp.tnodeid==thisTnodeid) & (df_temp.scenario==thisScenario)]
+					else:
+						filterDF=df[(df.tnodeid==thisTnodeid) & (df.scenario==thisScenario)]
+					thisData.append({'x':filterDF.t, 'y':filterDF.value, 'type': 'chart','name':"{}_{}".format(thisTnodeid,thisScenario)})
+					
 			figure['data']=thisData
 			figure['layout']['title']='{}'.format(prop.capitalize())
 			figure['layout']['xaxis']={'title':'Time (s)'}
 			figure['layout']['yaxis']={'title':'{} (PU)'.format(prop.capitalize())}
+		toc = time.clock()
+		print("Time taken to process dataframe:{:.2f} s".format(toc - tic))
 
 		return [figure,style]
 	except:
@@ -212,36 +234,100 @@ if __name__ == '__main__':
 
 	import sys
 	sys.path.insert(0,sys.argv[2])
-
+	
 	from tdcosim.dashboard import Dashboard
 	from tdcosim.data_analytics import DataAnalytics
 
-	# # init
-	df_file = os.path.join(sys.argv[1])
-	print(df_file)
-	if '.pkl' in sys.argv[1]:
-		df=pd.read_pickle(sys.argv[1])
-	elif '.csv' in sys.argv[1]:
-		df=pd.read_csv(sys.argv[1])
+	# # init	
+	nFiles = int(sys.argv[4])
+	useDask = sys.argv[5]
+	
+	tic = time.clock()
+	if os.path.isfile(sys.argv[1]):
+		print("{} is a file.".format(sys.argv[1]))
+		if '.pkl' in sys.argv[1]:
+			df=pd.read_pickle(sys.argv[1])
+		elif '.csv' in sys.argv[1]:
+			df=pd.read_csv(sys.argv[1])
+		else:
+			df=pd.read_pickle(sys.argv[1]) #Assume that df is saved as a pickle file by default.
+	
+	elif os.path.isdir(sys.argv[1]):
+		csvFileNames = []
+		pickleFileNames = []
+		simulationFolders = os.listdir(sys.argv[1])
+		print('Directory:{} contains {} simulation folders'.format(sys.argv[1],len(simulationFolders)))
+		
+		_ = [csvFileNames.extend(glob.glob(os.path.join(sys.argv[1],simulationFolder,"df_pickle.csv"))) for simulationFolder in simulationFolders]
+		print("Directory:{} contains {} CSV files!".format(sys.argv[1],len(csvFileNames),sys.argv[4]))
+		_ = [pickleFileNames.extend(glob.glob(os.path.join(sys.argv[1],simulationFolder,"df_pickle.pkl"))) for simulationFolder in simulationFolders if "df_pickle.csv" not in os.listdir(os.path.join(sys.argv[1],simulationFolder))]
+		print("Directory:{} contains {} Pickle files with no corresponding CSV files!".format(sys.argv[1],len(pickleFileNames),sys.argv[4]))
+		
+		if csvFileNames + pickleFileNames:
+			if len(csvFileNames) < nFiles:
+				pickleFileNames = pickleFileNames[0:nFiles-len(csvFileNames)] #Priority is for CSV files
+			else:
+				pickleFileNames = []
+			csvFileNames = csvFileNames[0:nFiles] #First n CSV files
+			print("First {} CSV files and {} Pickle files will be read.".format(len(csvFileNames),len(pickleFileNames)))
+			dfs = []
+			if useDask.lower()=="false" or not useDask: #Check if user wants to use Dask
+				if csvFileNames:
+					print("Concatenating {} CSV files into a single data frame using Pandas...".format(len(csvFileNames)))
+					#dfs.extend(pd.concat(map(pd.read_csv, csvFileNames), ignore_index=True))
+					dfs.append(pd.concat([pd.read_csv(csvFileName,dtype={'tnodesubid': 'object','tnodeid':'object','dfeederid':'object','dnodeid':'object','t':'float32','value':'float32'}) for csvFileName in csvFileNames],ignore_index=True))
+				if pickleFileNames:
+					print("Concatenating {} Pickle files into a single data frame using Pandas...".format(len(pickleFileNames)))
+					dfs.append(pd.concat([pd.read_pickle(pickleFileName,dtype={'tnodesubid': 'object','tnodeid':'object','dfeederid':'object','dnodeid':'object','t':'float32','value':'float32'}) for pickleFileName in pickleFileNames],ignore_index=True))
+				
+				df= pd.concat(dfs, ignore_index=True)
+				
+				print("Dataframe shape:{}".format(df.shape))
+			else:
+				if csvFileNames:
+					print("Concatenating {} CSV files into a single data frame using Dask...".format(len(csvFileNames)))
+					df = dd.read_csv(csvFileNames,dtype={'tnodesubid': 'object','tnodeid':'object','dfeederid':'object','dnodeid':'object','t':'float32','value':'float32'})
+					#dfs = [dd.read_csv(csvFileName,dtype={'tnodesubid': 'object','tnodeid':'object','dfeederid':'object','dnodeid':'object','t':'float32','value':'float32'}) for csvFileName in csvFileNames]
+					#df = dd.concat(dfs) #Another option
+				if pickleFileNames:
+					print("Pickle files will not be read using dask")
+				
+		else:
+			raise NotImplementedError("No CSV or Pickle files were found!")
 	else:
-		df=pd.read_pickle(sys.argv[1]) #Assume that df is saved as a pickle file by default.
+		raise ValueError("Only folder names or file names are accepted")
+	toc = time.clock()
+	print("Time taken to load CSV/Pickle files into dataframe:{:.2f} s".format(toc - tic))
+	
+	tic = time.clock()
 	if sys.argv[3].lower()=="true":# reduced memory
 		df=df[df.dfeederid.isna()]
 	da=DataAnalytics()
 	helper=Dashboard()
-	helper.add_df('df',df)
-
 	# setup objects
 	objects={'graph':{},'tab':{},'tabs':{},'upload':{},'textarea':{},'dropdown':{},'selection':{},'df':{}}
+	
+	if isinstance(df,dd.core.DataFrame):
+		objects['df_temp'] = df[(df.t>0.0) & (df.t<=0.01)].compute()
+		objects['scenariosFound'] = list(objects['df_temp'].scenario.unique())
+		helper.add_df('df',objects['df_temp'])
+	else:
+		objects['scenariosFound'] = list(df.scenario.unique())
+		helper.add_df('df',df)
+	print("Following scenarios were found from the CSV and Pickle files:{}".format(objects['scenariosFound']))
+	
 	objects['df']=df
-	objects['dfMap'] = helper.add_lat_lon_to_df(df[(df.property=='VOLT')], 'tnodeid')
-
+	
+	if isinstance(df,dd.core.DataFrame):
+		objects['df_scenario'] = df[df.scenario==objects['scenariosFound'][0]].compute()
+		objects['dfMap'] = helper.add_lat_lon_to_df(objects['df_scenario'][objects['df_scenario'].property=='VOLT'], 'tnodeid')
+	else:
+		objects['dfMap'] = helper.add_lat_lon_to_df(df[(df.property=='VOLT') & (df.scenario==objects['scenariosFound'][0])], 'tnodeid')
 	# define layout
 	gather_objects()
 	app.layout=html.Div(objects['tabs']['main_tab'],
 	style={'width':'99vw','height':'98vh','background-color':'rgba(0,0,0,.8)'})
-
+	toc = time.clock()
+	print("Time taken to process dataframe:{:.2f} s".format(toc - tic))
 	# run
 	app.run_server(debug=False,use_reloader=False)
-
-
